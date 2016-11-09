286f8991bc52a13561c9bcd0034d654e359c04df
Maximilian Berger <snowdragon92@gmail.com>
2016-11-09 11:03:40 +0100

    add mongo instance and user

--- examples/wiki/wiki.py
+++ examples/wiki/wiki.py
@@ -4,6 +4,9 @@ import markdown2
 import re
 
 app = Flask(__name__)
+app.config['MONGO_HOST'] = "ipraktikumtest_db_1"
+app.config['MONGO_USERNAME'] = "mberger"
+app.config['MONGO_PASSWORD'] = "12345"
 mongo = PyMongo(app)
 
 WIKIPART = re.compile(r'([A-Z][a-z0-9_]+)')
